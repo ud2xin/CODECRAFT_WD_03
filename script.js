3cdaa86d4f3abd2a24c984739241da620482ecd0
Muhammad Haikal Syamsuddin <udingaming0903@gmail.com>
2025-08-14 23:13:08 +0700

    .js for 2players tic-tac-toe will add ai tomorrow

---
+++ script.js
@@ -0,0 +1,75 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#status");
+const resetBtn = document.querySelector("#reset");
+
+let currentPlayer = "X";
+let board = ["", "", "", "", "", "", "", "", ""];
+let running = true;
+
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+cells.forEach(cell => cell.addEventListener("click", cellClicked));
+
+resetBtn.addEventListener("click", resetGame);
+
+statusText.textContent = `Player ${currentPlayer}'s turn`;
+
+function cellClicked() {
+    const cellIndex = this.getAttribute("data-index");
+    if (board[cellIndex] !== "" || !running) {
+        return;
+    }
+
+    updateCell(this, cellIndex);
+    checkWinner();
+}
+
+function updateCell(cell, index) {
+    board[index] = currentPlayer;
+    cell.textContent = currentPlayer;
+}
+
+function checkWinner() {
+    let winnerFound = false;
+
+    for (let i = 0; i < winConditions.length; i++) {
+        const [a, b, c] = winConditions[i];
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            winnerFound = true;
+            break;
+        }
+    }
+
+    if (winnerFound) {
+        statusText.textContent = `Player ${currentPlayer} wins! 🎉`;
+        running = false;
+    } else if (!board.includes("")) {
+        statusText.textContent = "It's a draw! 🤝"
+        running = false;
+    } else {
+        changePlayer();
+    }
+}
+
+function changePlayer() {
+    currentPlayer = (currentPlayer === "X") ? "O" : "X";
+    statusText.textContent = `Player ${currentPlayer}'s turn`;
+}
+
+
+function resetGame() {
+    currentPlayer = "X";
+    board = ["", "", "", "", "", "", "", "", ""];
+    running = true;
+    statusText.textContent = `Player ${currentPlayer}'s turn`;
+    cells.forEach(cell => cell.textContent = "");
+}
